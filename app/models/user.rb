34e8cc500bd759bcfe9680e30d77dcb26533fd2d
dgarate <garatex@gmail.com>
2021-01-17 18:03:18 -0300

    application before adding devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :co_user_role_cycles, dependent: :destroy
     has_many :companies, through: :co_user_role_cycles
     has_many :cycles, through: :co_user_role_cycles
